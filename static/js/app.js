f87f6a8a7e6de5fdd301afaadac0e8b48f785c45
Ryan Senkbeil <rsenk330@gmail.com>
2014-02-02 21:56:19 -0600

    Add empty pages for other routes.

--- static/js/app.js
+++ static/js/app.js
@@ -18,6 +18,9 @@
       $routeProvider.when('/', {templateUrl: PARTIALS_ROOT + '/home.html', controller: 'HomeController'});
       $routeProvider.when('/devices', {templateUrl: PARTIALS_ROOT + '/devices.html', controller: 'DeviceListController'});
       $routeProvider.when('/devices/:id', {templateUrl: PARTIALS_ROOT + '/device.html', controller: 'DeviceGetController'});
+      $routeProvider.when('/guide', {templateUrl: PARTIALS_ROOT + '/guide.html', controller: 'GuideController'});
+      $routeProvider.when('/recordings', {templateUrl: PARTIALS_ROOT + '/recordings.html', controller: 'RecordingsController'});
+      $routeProvider.when('/settings', {templateUrl: PARTIALS_ROOT + '/settings.html', controller: 'SettingsController'});
       $routeProvider.otherwise({redirectTo: '/'});
     }]);
 })();
